c26eddcd71fb0a717181b9a6440bf1ef10e1acbd
monadk <jadek.engr@gmail.com>
2025-06-19 16:01:03 +0900

    add streamlit.sh

--- streamlit.sh
+++ streamlit.sh
@@ -1,2 +1,3 @@
-pip install streamlit requests openai azure-ai-search azure-identity dotenv azure-search-documents azure-core
+pip install streamlit 
+
 python -m streamlit run app.py --server.port 8000 --server.address 0.0.0.0
